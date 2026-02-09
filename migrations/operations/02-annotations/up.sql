544f045df24124848e4da463bf03b25c97b74f8e
Chris Mitchell <chris.mit7@gmail.com>
2026-02-09 15:11:13 -0500

    Add sql

--- migrations/operations/02-annotations/up.sql
+++ migrations/operations/02-annotations/up.sql
@@ -2,7 +2,9 @@ CREATE TABLE annotation_files (
   id INTEGER PRIMARY KEY NOT NULL,
   operation_hash BLOB NOT NULL,
   file_addition_id BLOB NOT NULL,
+  index_file_addition_id BLOB,
   name TEXT,
   FOREIGN KEY(operation_hash) REFERENCES operations(hash),
-  FOREIGN KEY(file_addition_id) REFERENCES file_additions(id)
+  FOREIGN KEY(file_addition_id) REFERENCES file_additions(id),
+  FOREIGN KEY(index_file_addition_id) REFERENCES file_additions(id)
 ) STRICT;
