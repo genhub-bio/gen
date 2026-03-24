e7bfbc15dc9e051dad877eb086da189239df1e33
David Hofer <david.hofer@gmail.com>
2026-03-24 17:21:43 -0400

    Python fixes

--- gen-python/src/python_api/sequence_part.rs
+++ gen-python/src/python_api/sequence_part.rs
@@ -1,4 +1,4 @@
-use pyo3::pyclass;
+use pyo3::{prelude::*, pyclass};
 
 /// Exposes a SequencePart to Python.
 #[pyclass]
@@ -7,4 +7,16 @@ pub struct PySequencePart {
     pub name: String,
     pub sequence: String,
     pub sequence_length: i64,
+}
+
+#[pymethods]
+impl PySequencePart {
+    #[new]
+    fn new(name: String, sequence: String, sequence_length: i64) -> Self {
+        PySequencePart {
+            name,
+            sequence,
+            sequence_length,
+        }
+    }
 }
